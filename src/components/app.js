35cb55bfb35c17c0e4aaade582945e0bae246be6
Azim Saiyed <macbook@Azims-MacBook-Pro.local>
2017-05-15 17:20:55 -0400

    added react-router | removed custom router

---
+++ src/components/app.js
@@ -0,0 +1,24 @@
+/*
+* This is the top-level file for our application
+*/
+/*eslint-disable strict */
+
+var React = require('react');
+var Header = require('./common/header');
+var RouteHandler = require('react-router').RouteHandler;
+$ = jQuery = require('jquery');
+
+var App = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<Header/>
+				<div className="container-fluid">
+					<RouteHandler/>
+				</div>
+			</div>
+		);
+	}
+});
+
+module.exports = App;
--- src/main.js
+++ src/main.js
@@ -1,46 +1,53 @@
 //"use strict" --not using here coz of $ global var , but use IIFE to bypass the error as below
 
-$ = jQuery = require('jquery');
+"use strict";
 
 var React = require('react');
-var Home = require('./components/homePage');
-var Authors = require('./components/authors/authorPage');
-var About = require('./components/about/aboutPage');	
-var Header = require('./components/common/header');
+var Router = require('react-router');
+var routes = require('./routes');
 
-(function(win) {
-	"use strict";
-	var App = React.createClass({
-		render: function() {
-			var Child;
-
-			switch(this.props.route) {
-				case 'about' : Child = About; break;
-				case 'authors' : Child = Authors; break;
-				default: Child = Home; 
-			}
-
-			return (
-				<div>
-					<Header/>
-					<Child/>
-				</div>
-			);
-		}
-	});
-
-	function render() {
-		console.log("hashchange");
-		var route = window.location.hash.substr(1);
-		React.render(<App route={route} />, document.getElementById('app'));
-	}
-
-	win.addEventListener('hashchange',render);
-	render();
-
-})(window);
+Router.run(routes, function(Handler) {
+	React.render(<Handler/>, document.getElementById('app'));
+});
 
 /*
+NOTE: code for custom made ROUTING - without using React-Router {|||
+	$ = jQuery = require('jquery');
+
+	(function(win) {
+		"use strict";
+		var App = React.createClass({
+			render: function() {
+				var Child;
+
+				switch(this.props.route) {
+					case 'about' : Child = About; break;
+					case 'authors' : Child = Authors; break;
+					default: Child = Home; 
+				}
+
+				return (
+					<div>
+						<Header/>
+						<Child/>
+					</div>
+				);
+			}
+		});
+
+		function render() {
+			console.log("hashchange");
+			var route = window.location.hash.substr(1);
+			React.render(<App route={route} />, document.getElementById('app'));
+		}
+
+		win.addEventListener('hashchange',render);
+		render();
+
+	})(window);
+
+|||}
+
 React.render(<Home />, document.getElementById('app'));
 
 var App = console.log("Hello from browserify!!!!");
